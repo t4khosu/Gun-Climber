26bc89e4bffadf81c13b2e8db93fd65485d6182c
t4khosu <t4khosu@gmail.com>
2021-02-08 00:28:59 +0100

    Fix Platform and Slider enemy leave stage bug; Make Platform transparent

--- Assets/Scripts/MoveHorizontally.cs
+++ Assets/Scripts/MoveHorizontally.cs
@@ -6,11 +6,14 @@ public class MoveHorizontally : MonoBehaviour
 {
     [SerializeField] private LayerMask _solidLayer;
     [SerializeField] private float _speed;
+    [SerializeField] private bool _addSpacingBeforeCollision;
 
     private int _dir;
 
     private Rigidbody2D _rb2d;
     private float _width;
+    private float _leftBoundPixelPos;
+    private float _rightBoundPixelPos;
 
     public float Speed{
         set{_speed = value;}
@@ -22,6 +25,14 @@ public class MoveHorizontally : MonoBehaviour
         _dir = Random.Range(0, 2) * 2 - 1;
         _rb2d = GetComponent<Rigidbody2D>();
         _width = GetComponent<SpriteRenderer>().bounds.size.x;
+
+        _leftBoundPixelPos = WorldGenerator.WG.LeftBound * WorldGenerator.WG.CellSize.x + WorldGenerator.WG.CellSize.x + _width / 2;
+        _rightBoundPixelPos = WorldGenerator.WG.RightBound * WorldGenerator.WG.CellSize.x - _width / 2;
+
+        if(_addSpacingBeforeCollision){
+            _leftBoundPixelPos += 0.8f * WorldGenerator.WG.CellSize.x;
+            _rightBoundPixelPos -= 0.8f * WorldGenerator.WG.CellSize.x;
+        }
     }
 
     void FixedUpdate()
@@ -31,13 +42,19 @@ public class MoveHorizontally : MonoBehaviour
     }
 
     void CheckForDirectionChange(){
-        Vector2 pointOnBorder = new Vector2(transform.position.x + (_width / 2) * _dir, transform.position.y);
-
-        RaycastHit2D hit = Physics2D.Raycast(pointOnBorder, Vector2.right * _dir, 1f, _solidLayer);
-
-        if(hit.collider != null){
+        if (WillCollideWithBlock() || WillLeaveSideBounds()){
             _dir *= -1;
         }
+    }
+
+    bool WillCollideWithBlock(){
+        Vector2 pointOnBorder = new Vector2(transform.position.x + (_width / 2) * _dir, transform.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(pointOnBorder, Vector2.right * _dir, 1f, _solidLayer);
+        return hit.collider != null;
+    }
+
+    bool WillLeaveSideBounds(){
+        return transform.position.x <= _leftBoundPixelPos || transform.position.x >= _rightBoundPixelPos;
     }
 
     void Move(){
